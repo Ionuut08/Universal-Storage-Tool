f285d5382aac5f5e703c1db3a970a45272b1cb6c
Ionuut08 <ionut.feraru@info.uaic.ro>
2020-04-17 10:02:02 +0300

    starting

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,43 +3,43 @@ class LoginsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def new
-    redirect_to users_path if current_user
+    redirect_to users_path # if current_user
   end
 
   def create
-    user = authenticate_with_google
-    if user
-      sign_in user
+    # user = authenticate_with_google
+    # if user
+    #   sign_in user
       redirect_to users_path
-    else
-      raise ActionController::RoutingError, 'Forbidden'
-    end
-  end
+  #   else
+  #     raise ActionController::RoutingError, 'Forbidden'
+  #   end
+  # end
 
   def logout
-    sign_out current_user
+    # sign_out current_user
     redirect_to root_path
   end
 
   private
 
   def authenticate_with_google
-    if id_token = flash[:google_sign_in_token]
-      google_token = GoogleSignIn::Identity.new(id_token)
-      user = User.where(google_id: google_token.user_id).first
-      if user
-        user.update(formatted_google_params(google_token))
-        $session = GoogleDrive::Session
+    # if id_token = flash[:google_sign_in_token]
+    #   google_token = GoogleSignIn::Identity.new(id_token)
+    #   user = User.where(google_id: google_token.user_id).first
+    #   if user
+    #     user.update(formatted_google_params(google_token))
+    #     $session = GoogleDrive::Session
                        .login_with_oauth(google_token)
-        user
-      else
-        User.create(formatted_google_params(google_token))
-      end
-    elsif error = flash[:google_sign_in][:error]
-      logger.error "Google authentication error: #{error}"
-      nil
-    end
-  end
+  #       user
+  #     else
+  #       User.create(formatted_google_params(google_token))
+  #     end
+  #   elsif error = flash[:google_sign_in][:error]
+  #     logger.error "Google authentication error: #{error}"
+  #     nil
+  #   end
+  # end
 
   def formatted_google_params(token)
     {
